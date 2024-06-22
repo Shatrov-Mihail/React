8c324fb6cea9aa78fd8116815ac76c0cdbf3dff2
Shatrov-Mihail <78844887@mail.ru>
2024-06-22 23:23:36 +0500

    Без использования JSX

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
+import React, { createElement } from 'react';
 import logo from "./logo.svg";
 import "./App.css";
 
 export const App = () => {
-  const currentYear = new Date().getFullYear(); //Императивный стиль
+	const currentYear = new Date().getFullYear(); //Императивный стиль
 	//Декларативный стиль начало
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <footer className="footer">Текущий год: {currentYear}</footer>
-    </div>
-  );
+	return React.createElement("div", {
+	  className: "App"
+	}, React.createElement("header", {
+	  className: "App-header"
+	}, React.createElement("img", {
+	  src: logo,
+	  className: "App-logo",
+	  alt: "logo"
+	}), React.createElement("p", null, "Edit ", React.createElement("code", null, "src/App.js"), " and save to reload."), React.createElement("a", {
+	  className: "App-link",
+	  href: "https://reactjs.org",
+	  target: "_blank",
+	  rel: "noopener noreferrer"
+	}, "Learn React")), React.createElement("footer", {
+	  className: "footer"
+	}, "\u0422\u0435\u043A\u0443\u0449\u0438\u0439 \u0433\u043E\u0434: ", currentYear));
 	// Декларативный стиль конец
-};
+  };
